d93fa3cdf4f85fdfc931a07d16b3f1f7384487a0
rossedlin <rossedlin@gmail.com>
2017-03-03 16:13:50 +0000

    update to include domain

--- app/Api/Plugin/Google/Analytics/ExcludeIP.php
+++ app/Api/Plugin/Google/Analytics/ExcludeIP.php
@@ -95,36 +95,126 @@ class ExcludeIP extends _Plugin
 		}
 
 		/**
-		 * Get the IP from the Request
+		 * Get the Domain/IP from the Request
 		 */
-		$ip = $request->get('ip', Core\Request::server('REMOTE_ADDR'));
+		$ip     = $request->get('ip', false);
+		$domain = $request->get('domain', false);
 
 		/**
-		 * Validate the IP
+		 * Validate the Domain/IP
+		 * Retrieve the record if it exists
 		 */
-		if (!Core\Utils::isValidIp($ip))
+		if (self::isValidDomain($domain))
 		{
-			abort(404);
+			$this->postDomain($domain);
+		}
+		elseif (Core\Utils::isValidIp($ip))
+		{
+			$this->postIp($ip);
+		}
+		elseif (Core\Utils::isValidIp(Core\Request::server('REMOTE_ADDR')))
+		{
+			$this->postIp(Core\Request::server('REMOTE_ADDR'));
 		}
 
 		/**
-		 * Retrieve the record if it exists
-		 * Insert/Update the IP
+		 * Neither IP/Domain so lets die
 		 */
+		$this->render([
+			'success' => false,
+			'message' => 'Bad IP/Domain',
+		]);
+	}
+
+	/**
+	 * @param string $ip
+	 */
+	private function postIp($ip)
+	{
 		$googleAnalyticsIp = Models\GoogleAnalyticsIp::where('address', $ip)->first();
 
+		/**
+		 * Check $googleAnalyticsIp
+		 */
 		if (!($googleAnalyticsIp instanceof Models\GoogleAnalyticsIp))
 		{
-			$googleAnalyticsIp = new Models\GoogleAnalyticsIp;
+			$googleAnalyticsIp          = new Models\GoogleAnalyticsIp;
+			$googleAnalyticsIp->address = $ip;
 		}
 
-		$googleAnalyticsIp->address = $ip;
-		$googleAnalyticsIp->type    = Models\GoogleAnalyticsIp::TYPE_IP;
-		$googleAnalyticsIp->save();
+		$googleAnalyticsIp->type = Models\GoogleAnalyticsIp::TYPE_IP;
+
+		/**
+		 * Insert/Update Record
+		 */
+		$success = $googleAnalyticsIp->save();
 
 		$this->render([
-			'success'      => $googleAnalyticsIp->save(),
-			'ip' => $ip,
+			'success' => $success,
+			'ip'      => $ip,
 		]);
+	}
+
+	/**
+	 * @param string $domain
+	 */
+	private function postDomain($domain)
+	{
+		$googleAnalyticsIp = Models\GoogleAnalyticsIp::where('address', $domain)->first();
+
+		/**
+		 * Check $googleAnalyticsIp
+		 */
+		if (!($googleAnalyticsIp instanceof Models\GoogleAnalyticsIp))
+		{
+			$googleAnalyticsIp          = new Models\GoogleAnalyticsIp;
+			$googleAnalyticsIp->address = $domain;
+		}
+
+		$googleAnalyticsIp->type = Models\GoogleAnalyticsIp::TYPE_DOMAIN;
+
+		/**
+		 * Insert/Update Record
+		 */
+		$success = $googleAnalyticsIp->save();
+
+		$this->render([
+			'success' => $success,
+			'domain'  => $domain,
+		]);
+	}
+
+	/**
+	 * @param string $domain
+	 *
+	 * @return bool
+	 */
+	private static function isValidDomain($domain)
+	{
+		/**
+		 * Valid chars check
+		 */
+		if (!preg_match('/^([a-z\d](-*[a-z\d])*)(\.([a-z\d](-*[a-z\d])*))*$/i', $domain))
+		{
+			return false;
+		}
+
+		/**
+		 * Overall length check
+		 */
+		if (!preg_match('/^.{1,253}$/', $domain))
+		{
+			return false;
+		}
+
+		/**
+		 * Length of each label
+		 */
+		if (!preg_match('/^[^\.]{1,63}(\.[^\.]{1,63})*$/', $domain))
+		{
+			return false;
+		}
+
+		return true;
 	}
 }
